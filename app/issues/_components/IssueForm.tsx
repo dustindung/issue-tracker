0d5681b386f7a3862c8532d86ed2a425cea2b49d
Dustin <dustindung300@gmail.com>
2023-10-19 21:01:19 +0700

    Fix caching issue.

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -40,6 +40,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
       }
 
       router.push("/issues");
+      router.refresh();
     } catch (error) {
       setSubmitting(false);
       setError("An unexpected error occurred.");
