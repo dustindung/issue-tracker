5034086674cbed0549781b4802f7a106827f766d
Dustin <dustindung300@gmail.com>
2023-10-29 14:34:52 +0700

    Add metadata.

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -4,6 +4,7 @@ import Pagination from "../components/Pagination";
 import IssueActions from "./IssueActions";
 import IssueTable, { IssueQuery, columnValues } from "./IssueTable";
 import { Flex } from "@radix-ui/themes";
+import { Metadata } from "next";
 
 interface Props {
   searchParams: IssueQuery;
@@ -48,3 +49,8 @@ const IssuesPage = async ({ searchParams }: Props) => {
 export default IssuesPage;
 
 export const dynamic = "force-dynamic";
+
+export const metadata: Metadata = {
+  title: "Issue Tracker - List",
+  description: "View all project summary",
+};
